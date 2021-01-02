27eedfc413455d18e1c2dae5da6c21d04d3a94c9
Ramon Hubbell <Ramon.hubbell@gmail.com>
2021-01-02 12:43:32 -0600

    feature: working on two way binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UsernameComponent } from './username/username.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsernameComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
