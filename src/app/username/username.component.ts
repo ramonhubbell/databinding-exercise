67278d4fc125339364308da7538c427d6e891bcf
Ramon Hubbell <Ramon.hubbell@gmail.com>
2021-01-03 11:49:13 -0600

    feature: finished username reset app

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -7,14 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsernameComponent implements OnInit {
   userName = '';
+  // isFieldEmpty = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onUpdateUsername (event: Event){
-    this.userName = (<HTMLInputElement>event.target).value;
-  }
+  // onUpdateUsername (event: Event){
+  //   this.userName = (<HTMLInputElement>event.target).value;
+  // }
+
+  // activateButton(){
+  //   this.isFieldEmpty = true;
+  //   console.log(this.isFieldEmpty);
+  // }
+
+  // resetUserName(){
+  //   this.userName = '';
+  // }
 
 }
